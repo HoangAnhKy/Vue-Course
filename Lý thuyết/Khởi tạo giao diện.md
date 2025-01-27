34dd3dae652f3cdcf3e4c5e56f9786bd329d7625
HoangAnhKy <kyha-ts@hikesiya.co.jp>
2025-01-28 01:37:29 +0700

    update computed

--- Lý thuyết/Khởi tạo giao diện.md
+++ Lý thuyết/Khởi tạo giao diện.md
@@ -71,3 +71,31 @@ app.mount('#app');
 </script>
 
 ```
+
+cách sử dụng thứ 2
+
+```vue
+<script setup>
+  import {computed, ref} from "vue";
+  const count = ref(0)
+
+  function increase() {
+    count.value++;
+  }
+
+  const checkNumber = computed( () =>  {
+    console.log("cache") // loop liên tục nếu ko có computed
+    return count.value <= 2 ? "count <= 2" : "count > 2"
+  })
+</script>
+
+<template>
+  <h1 ref="elementRef">{{ count }}</h1>
+  <h1>{{ checkNumber }}</h1>
+  <h3></h3>
+
+  <button @click="increase">+ </button>
+</template>
+
+
+```
--- Thực hành/course_vue/src/App.vue
+++ Thực hành/course_vue/src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
-    import {ref, reactive, nextTick} from "vue";
+import {computed, ref} from "vue";
     const count = ref(0)
 
     function increase() {
         count.value++;
-        console.log(this.$refs.elementRef)
     }
+
+    const checkNumber = computed( () =>  {
+        console.log("cache")
+        return count.value <= 2 ? "count <= 2" : "count > 2"
+    })
 </script>
 
 <template>
     <h1 ref="elementRef">{{ count }}</h1>
+    <h1>{{ checkNumber }}</h1>
+    <h3></h3>
 
     <button @click="increase">+ </button>
 </template>
