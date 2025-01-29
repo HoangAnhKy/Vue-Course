7e21437bac9c553f4e2f97f6d9b27f30dbf29d22
HoangAnhKy <kyha-ts@hikesiya.co.jp>
2025-01-29 20:58:38 +0700

    v modal

---
+++ Thực hành/course_vue/src/Course.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { nextTick, ref } from "vue";
+
+let bind_A = ref("mặc");
+let bind_B = ref("định");
+
+const changeA = (e) => {
+    bind_A = e.target.value
+    console.log(e.target.value)
+};
+</script>
+
+<template>
+    <label>{{ bind_A }}</label> <br />
+    <label>{{ bind_B }}</label>
+
+    <input v-model="bind_B">
+
+    <input @change="changeA">
+</template>
